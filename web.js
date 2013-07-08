24c3c993b8133db7508281f9408fa5a05e83d0b6
Jivko Kolev <jivkokgit@gmail.com>
2013-07-08 19:41:05 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var fs = require('fs');
+var buffer = fs.readFile("index.html");
+var s = buffer.toString();
 
+var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(s);
 });
 
 var port = process.env.PORT || 5000;
